ee861675735f6c3c5094f7085fc2575034f3a2e1
jlaws710 <jlaws710@gmail.com>
2023-02-18 12:13:15 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Navigation } from './components/Navigation';
 import { PokemonCard } from './components/PokemonCard';
 
 const LIMIT = 150;
 const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
 
+// function Pokimon(props) {
+//   const [data, setData] = useState([])
+//   const getData = async () => {
+//     const response = await fetch(pokeApi)
+//     const pokiData = await response.json();
+
+//     data = pokiData;
+//     setData(data)
+//   }
+
+//   useEffect( () => {
+//     getData();
+//   }, [] )
+// }
 function App() {
+  const [data, setData] = useState('');
+  // const pokiData = fetch(pokeApi);
+  // data = pokiData;
+  // setData(data);
+
+  useEffect(() => {
+    fetch(pokeApi)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+  console.log(data)
 
   return (
     <div data-testid="app">
       <Navigation />
-
-      <h1>Pokemon should appear here</h1>
+      
+      <h1> Pokemon should appear here </h1>
       <PokemonCard />
     </div>
   );
