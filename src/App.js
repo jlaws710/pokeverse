4c65cfc9bcc7c377cb94e30ec44fc1fd693c26c7
jlaws710 <jlaws710@gmail.com>
2023-02-24 14:48:29 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -5,41 +5,26 @@ import { PokemonCard } from './components/PokemonCard';
 const LIMIT = 150;
 const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
 
-// function Pokimon(props) {
-//   const [data, setData] = useState([])
-//   const getData = async () => {
-//     const response = await fetch(pokeApi)
-//     const pokiData = await response.json();
-
-//     data = pokiData;
-//     setData(data)
-//   }
-
-//   useEffect( () => {
-//     getData();
-//   }, [] )
-// }
 function App() {
-  const [data, setData] = useState('');
-  // const pokiData = fetch(pokeApi);
-  // data = pokiData;
-  // setData(data);
+  const [pokeCards, setPokeCards] = useState([]);
 
   useEffect(() => {
-    fetch(pokeApi)
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-      });
-  }, []);
-  console.log(data)
-
+  fetch(pokeApi)
+  .then((res) => res.json())
+  .then((data) => {
+    setPokeCards(data.results);
+    console.log(data.results);
+});
+}, []);
   return (
     <div data-testid="app">
       <Navigation />
-      
+    
       <h1> Pokemon should appear here </h1>
-      <PokemonCard />
+      {pokeCards.length > 0 && 
+        pokeCards.map((card) => {
+          <PokemonCard name={card.name} url={card.url} />;
+      })}
     </div>
   );
 }
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Card } from 'react-bootstrap';
 
 function PokemonCard({ url, name }) {
+  const [pokemon, setPokemon] = useState([null]);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setPokemon(data);
+        console.log(data);
+      });
+  }, [url]);
 
   return (
-    <div>
-        pokemon card
-    </div>
+    <>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={pokemon?.sprites.front_default} />
+        <Card.Body>
+          <Card.Title>{name}</Card.Title>
+        </Card.Body>
+      </Card>
+    </>
   );
 }
 
